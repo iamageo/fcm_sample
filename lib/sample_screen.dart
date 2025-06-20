9342eeccca959bc371ad53fdfdadb70d8c1f5ff7
iamageo <geovani.af4@gmail.com>
2025-06-20 11:54:09 -0300

    ✨feat: Integrate Firebase Core and Messaging

---
+++ lib/sample_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SampleScreen extends StatefulWidget {
+  const SampleScreen({super.key});
+
+  @override
+  State<SampleScreen> createState() => _SampleScreenState();
+}
+
+class _SampleScreenState extends State<SampleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter FCM Sample"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Made with ❤️ by iamageo"),
+        ],
+      ),
+    );
+  }
+}
